2710a19c004382788f2d4edc5ef5da405d2be783
MaxenceCodeFR <maxence.hattabi@gmail.com>
2022-12-18 12:59:03 +0100

    Modification de CKeditor et un peu de css pour rendre ça joli

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -3,11 +3,17 @@
 namespace App\Form;
 
 use App\Entity\Annonce;
+use App\Entity\ExternaticConsultant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+
+use function Sodium\add;
 
 class AnnonceType extends AbstractType
 {
@@ -21,13 +27,25 @@ class AnnonceType extends AbstractType
             ->add('studyLevel')
             ->add('remote')
             ->add('workTime')
-            ->add('endingAt')
             ->add('createdAt', DateType::class, array(
                 'widget' => 'single_text',
                 'format' => 'yyyy-MM-dd',
             ))
+            ->add('endingAt', DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ))
+
             ->add('techno')
-            ->add('description', CKEditorType::class);
+            ->add('description', CKEditorType::class, [
+            'config_name' => 'light'
+            ])
+            ->add('author', EntityType::class, [
+                'class' => ExternaticConsultant::class,
+                'choice_label' => function (ExternaticConsultant $consultant) {
+                    return $consultant->getUser()->getFirstname();
+                }
+            ]);
     }
     public function configureOptions(OptionsResolver $resolver): void
     {
