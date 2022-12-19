c885ece43d222df5b205a60bb2ebf45c24c86250
e-riff <emeric.riff@gmail.com>
2022-12-19 11:31:45 +0100

    fetching annonces OK without other parameters. TODO : add other parameters (such as pay, working time, etc) in next US

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -6,16 +6,19 @@ use App\Repository\AnnonceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 #[route('/search', name: "search_")]
 class AnnonceController extends AbstractController
 {
     #[Route('/results', name: 'results')]
-    public function index(AnnonceRepository $annonceRepository): Response
+    public function index(Request $request, AnnonceRepository $annonceRepository): Response
     {
-        $annonces = $annonceRepository->findAll();
+        $fetchedAnnonces = is_null($request->get('form')['searchQuery']) ?
+            $annonceRepository->annonceFinder("*") :
+            $annonceRepository->annonceFinder($request->get('form')['searchQuery']);
         return $this->render('annonce/results.html.twig', [
-            'annonces' => $annonces,
+            'fetchedAnnonces' => $fetchedAnnonces,
         ]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -59,7 +59,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('registration/register.html.twig', [
