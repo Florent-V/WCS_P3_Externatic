34fb610b474b8372091afeaa4dcbb95798bda385
MaxenceCodeFR <maxence.hattabi@gmail.com>
2022-12-19 10:24:46 +0100

    oui

---
+++ migrations/Version20221219091919.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221219091919 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE candidat DROP phone');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL, ADD phone VARCHAR(45) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE candidat ADD phone VARCHAR(45) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP is_verified, DROP phone');
+    }
+}
