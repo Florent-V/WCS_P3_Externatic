512eb0e79f35be7ab4308c1f5bf1528328d44bc5
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-01-09 17:14:04 +0100

    pas derreur mais ça marche pas

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Company;
+use App\Entity\Message;
 use App\Entity\User;
+use App\Form\MessageType;
 use App\Repository\CandidatRepository;
+use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -44,10 +48,21 @@ class CompanyController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_company_show', methods: ['GET'])]
-    public function show(Company $company): Response
+    public function show(Company $company, Request $request, MessageRepository $messageRepository): Response
     {
-        return $this->render('company/show.html.twig', [
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $messageRepository->save($message, true);
+
+            $this->addFlash('success', 'Vous avez postulé !');
+            return $this->redirectToRoute('app_company_show', ['id' => $company->getId()]);
+        }
+        return $this->renderForm('company/show.html.twig', [
             'company' => $company,
+            'form' => $form,
         ]);
     }
 }
