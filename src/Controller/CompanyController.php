63ad2cb5b46972504ed563d5470d49eacbe908ce
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-01-10 11:36:37 +0100

    la libérations jésus reviens

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Company;
 use App\Entity\Message;
 use App\Entity\User;
-use App\Form\MessageType;
+use App\Form\SpontaneType;
 use App\Repository\CandidatRepository;
 use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use DateTime;
 
 #[Route('/company')]
 class CompanyController extends AbstractController
@@ -47,16 +48,24 @@ class CompanyController extends AbstractController
         return $this->render('company/favorites.html.twig');
     }
 
-    #[Route('/{id}', name: 'app_company_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_company_show', methods: ['GET', 'POST'])]
     public function show(Company $company, Request $request, MessageRepository $messageRepository): Response
     {
         $message = new Message();
-        $form = $this->createForm(MessageType::class, $message);
+        $form = $this->createForm(SpontaneType::class, $message);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $messageRepository->save($message, true);
+            $message->setSendTo($company->getExternaticConsultant()->getUser());
+            /**
+             * @var ?User $user
+             */
+            $user = $this->getUser();
+            $message->setSendBy($user);
 
+            $date = new DateTime();
+            $message->setDate($date);
+            $messageRepository->save($message, true);
             $this->addFlash('success', 'Vous avez postulé !');
             return $this->redirectToRoute('app_company_show', ['id' => $company->getId()]);
         }
