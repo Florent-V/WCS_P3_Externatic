5a86d33470f79295ec26c85c0aa955e1ce498216
e-riff <emeric.riff@gmail.com>
2023-01-15 14:54:40 +0100

    removing footer for consultants

--- assets/controllers/consultantBoardMargin_controller.js
+++ assets/controllers/consultantBoardMargin_controller.js
@@ -13,6 +13,8 @@ export default class extends Controller {
                     document.querySelector("main").style.marginLeft = "0rem"
                 }, 500);
             });
+
+            document.querySelector("footer").style="display:none;";
         }
     }
 }
