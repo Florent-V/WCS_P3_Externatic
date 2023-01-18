6cca0aef91efaf1c6a06daf248eca7e385fb40af
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2023-01-18 14:56:26 +0100

    add new Techno from AnnonceType with LiveComponent

--- src/Component/AnnonceForm.php
+++ src/Component/AnnonceForm.php
@@ -3,34 +3,41 @@
 namespace App\Component;
 
 use App\Entity\Annonce;
+use App\Entity\Techno;
 use App\Form\AnnonceType;
 use App\Repository\AnnonceRepository;
 use App\Repository\TechnoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
-use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
-use Symfony\UX\LiveComponent\LiveCollectionTrait;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[AsLiveComponent("annonce_form")]
 class AnnonceForm extends AbstractController
 {
     use DefaultActionTrait;
-    use LiveCollectionTrait;
+    use ComponentWithFormTrait;
 
     public function __construct(
-        private readonly TechnoRepository $technoRepository,
-        private readonly AnnonceRepository $annonceRepository
+        private readonly TechnoRepository $technoRepository
     ) {
     }
 
-    #[LiveProp(fieldName: 'annonceField')]
+    #[LiveProp(
+        writable: true,
+        dehydrateWith: 'dehydrateWith',
+        fieldName: 'annonceField'
+    )]
     #[Assert\Valid]
     public ?Annonce $annonce = null;
+
+    #[LiveProp(writable: true)]
+    public ?string $newTechnoName = null;
 
     #[LiveProp(writable: true)]
     public ?string $search = null;
@@ -40,28 +47,18 @@ class AnnonceForm extends AbstractController
         return $this->createForm(AnnonceType::class, $this->annonce);
     }
 
-    #[LiveAction]
-    public function addTechno(#[liveArg] ?string $name): void
+    public function dehydrateWith(): void
     {
-        if ($name) {
-            $this->annonceRepository->save($this->annonce, true);
-            $techno = $this->technoRepository->findOneBy(['name' => $name]);
-            $this->annonce->addTechno($techno);
-            $this->annonceRepository->save($this->annonce, true);
-            $this->formValues['techno'][] = ['name' => $name];
-        } else {
-            $this->formValues['techno'][] = [''];
+    }
+
+    #[LiveAction]
+    public function addTechno(): void
+    {
+        if ($this->newTechnoName) {
+            $techno = new Techno();
+            $techno->setName($this->newTechnoName);
+            $this->technoRepository->save($techno, true);
+            $this->newTechnoName = null;
         }
-    }
-
-    #[LiveAction]
-    public function removeTechno(#[liveArg] int $index): void
-    {
-        unset($this->formValues['techno'][$index]);
-    }
-
-    public function getResults(): array
-    {
-        return $this->search ? $this->technoRepository->search($this->search) : [];
     }
 }
