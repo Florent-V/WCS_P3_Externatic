d899843627ab8a9bf48f41d9174341f2932c410b
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-01-18 14:04:57 +0100

    css conversation

--- src/Form/ConversationType.php
+++ src/Form/ConversationType.php
@@ -6,6 +6,8 @@ use App\Entity\Message;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +16,7 @@ class ConversationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', CKEditorType::class, [
-                'config_name' => 'light',
-                'config'      => ['editorplaceholder' => "Taper votre message ici"]
-            ])
-            ->add('Envoyer', SubmitType::class);
+            ->add('content', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
