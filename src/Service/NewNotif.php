dc750d9331103bcb3a009248bcdbe5d9e21085fc
Nicolas Montard <montardnico@gmail.com>
2023-01-18 11:32:17 +0100

    envoie de notif en correspondant a searchProfile

--- src/Service/NewNotif.php
+++ src/Service/NewNotif.php
@@ -4,9 +4,10 @@ namespace App\Service;
 
 use App\Entity\Annonce;
 use App\Entity\Notif;
+use App\Entity\SearchProfile;
 use App\Repository\CandidatRepository;
 use App\Repository\NotifRepository;
-use App\Repository\UserRepository;
+use App\Repository\SearchProfileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
@@ -14,27 +15,27 @@ use DateTime;
 
 class NewNotif extends AbstractController
 {
-    private UserRepository $userRepository;
     private NotifRepository $notifRepository;
     private MailerInterface $mailer;
     private CandidatRepository $candidatRepository;
+    private SearchProfileRepository $profileRepository;
     public function __construct(
-        UserRepository $userRepository,
         NotifRepository $notifRepository,
         MailerInterface $mailer,
-        CandidatRepository $candidatRepository
+        CandidatRepository $candidatRepository,
+        SearchProfileRepository $profileRepository
     ) {
-        $this->userRepository = $userRepository;
         $this->notifRepository = $notifRepository;
         $this->mailer = $mailer;
         $this->candidatRepository = $candidatRepository;
+        $this->profileRepository = $profileRepository;
     }
 
     public function notifNewAnnonce(Annonce $annonce): void
     {
         $sentNotif = [];
+        $this->annonceByCriteria($annonce, $sentNotif);
         $this->annonceFavCompany($annonce, $sentNotif);
-        $this->annonceAll($annonce, $sentNotif);
     }
 
     private function annonceFavCompany(Annonce $annonce, array &$sentNotif): void
@@ -67,30 +68,52 @@ class NewNotif extends AbstractController
         }
     }
 
-    private function annonceAll(Annonce $annonce, array &$sentNotif): void
+    private function annonceByCriteria(Annonce $annonce, array &$sentNotif): void
     {
-        foreach ($this->userRepository->findByRole('ROLE_CANDIDAT') as $user) {
-            if (!in_array($user->getId(), $sentNotif)) {
-                array_push($sentNotif, $user->getId());
+        foreach ($this->profileRepository->findAll() as $searchProfile) {
+            if (
+                ($searchProfile->getSearchQuery()['salaryMin'] == "" ||
+                    $searchProfile->getSearchQuery()['salaryMin'] >= $annonce->getSalaryMin()) &&
+                ($searchProfile->getSearchQuery()['remote'] == "" ||
+                    $searchProfile->getSearchQuery()['remote'] == $annonce->isRemote()) &&
+                ($searchProfile->getSearchQuery()['company'] == "" ||
+                    $searchProfile->getSearchQuery()['company'] == $annonce->getCompany()->getId()) &&
+                ($this->annonceByCriteriaIf($searchProfile, $annonce))
+            ) {
+                array_push($sentNotif, $searchProfile->getCandidat()->getUser()->getId());
                 $notification = new Notif();
                 $notification->setContent($annonce->getTitle());
                 $notification->setType('newAnnonce');
                 $notification->setCreatedAt(new DateTime('now'));
-                $notification->setUser($user);
+                $notification->setUser($searchProfile->getCandidat()->getUser());
                 $notification->setParameter($annonce->getId());
                 $notification->setWasRead(false);
                 $this->notifRepository->save($notification, true);
 
                 $email = (new Email())
-                    ->to($user->getEmail())
+                    ->to($searchProfile->getCandidat()->getUser()->getEmail())
                     ->from($this->getParameter('mailer_from'))
-                    ->subject('Une nouvelle Annonce correspondant à vos critère a été créer')
+                    ->subject('Une nouvelle Annonce correspondant a vos critère vient d\'étre créer')
                     ->html($this->renderView('annonce/newEmail.html.twig', ['annonce' => $annonce]));
 
                 $this->mailer->send($email);
                 // sleep pour ne pas dépasser la limite de mailtrap, a enlever en prod
                 sleep(3);
             }
+        }
+    }
+    private function annonceByCriteriaIf(SearchProfile $searchProfile, Annonce $annonce): bool
+    {
+        if (
+            ($searchProfile->getSearchQuery()['searchQuery'] == "" ||
+                strpos($annonce->getTitle(), $searchProfile->getSearchQuery()['searchQuery'])) &&
+            ($searchProfile->getSearchQuery()['workTime'] == "" ||
+                $searchProfile->getSearchQuery()['workTime'] == 1 && $annonce->getWorkTime()->h > 35 ||
+                $searchProfile->getSearchQuery()['workTime'] == 0 && $annonce->getWorkTime()->h < 35)
+        ) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
