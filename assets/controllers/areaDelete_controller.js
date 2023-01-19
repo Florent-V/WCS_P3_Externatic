bf5053895901a9618bc664bfefc315318793c282
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-01-19 16:50:33 +0100

    fini ?

---
+++ assets/controllers/areaDelete_controller.js
@@ -0,0 +1,10 @@
+/*import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    connect() {
+        this.element.addEventListener('submit',() =>{
+            let textarea = this.element.firstElementChild;
+                textarea.value = '';
+        });
+    }
+} */
