8a409b26e836e8d83e1ebc52bb4b12aa3f78b55c
e-riff <emeric.riff@gmail.com>
2023-01-23 09:56:27 +0100

    button error ok

--- assets/controllers/inbox_controller.js
+++ assets/controllers/inbox_controller.js
@@ -1,8 +1,29 @@
 import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
-    archive(event)
-    {
+    static targets = ["error"]
+
+    closeModal(event) {
+        console.log("cocou")
+        this.errorTarget.close();
+    }
+
+    delete(event) {
+        fetch(`/recruitment_process/${event.currentTarget.dataset.messageid}/change-archive`)
+            .then(response => response.json())
+            .then((response) => {
+                if (response.isArchived === true) {
+                    event.target.closest("tr").remove()
+                } else {
+                    event.target.className = "bi bi-exclamation-diamond-fill";
+                    event.target.title = "erreur";
+                    this.errorTarget.show();
+                    //this.errorTarget.display = "block"
+                }
+            })
+    }
+
+    archive(event) {
         fetch(`/recruitment_process/${event.currentTarget.dataset.messageid}/change-read`)
             .then(response => response.json())
             .then((response) => {
@@ -20,20 +41,6 @@ export default class extends Controller {
             })
     }
 
-    delete(event) {
-        fetch(`/recruitment_process/${event.currentTarget.dataset.messageid}/change-archive`)
-            .then(response => response.json())
-            .then((response) => {
-                if (response.isArchived === true) {
-                    event.target.closest("tr").remove()
-                } else {
-                    event.target.className = "bi bi-exclamation-diamond-fill";
-                    event.target.title = "erreur";
-                }
-            })
-    }
-
-
     connect() {
         const messageCells = document.querySelectorAll("td:not(:has(> button))");
         for (let messageCell of messageCells) {
@@ -41,24 +48,5 @@ export default class extends Controller {
                 window.location.assign(` /message/recruitmentProcess/${messageCell.parentNode.dataset.messageid}`);
             })
         }
-
-
-        /*                let favoriteOfferLink = event.currentTarget;
-        let link = favoriteOfferLink.href;
-        // Send an HTTP request with fetch to the URI defined in the href
-        fetch(link)
-            // Extract the JSON from the response
-            .then(res => res.json())
-            // Then update the icon
-            .then(function (res) {
-                let bookmarkIcon = favoriteOfferLink.firstElementChild;
-                if (res.isInFavorite) {
-                    bookmarkIcon.classList.remove('bi-bookmark'); // Remove the .bi-heart (empty heart) from classes in <i> element
-                    bookmarkIcon.classList.add('bi-bookmark-fill'); // Add the .bi-heart-fill (full heart) from classes in <i> element
-                } else {
-                    bookmarkIcon.classList.remove('bi-bookmark-fill'); // Remove the .bi-heart-fill (full heart) from classes in <i> element
-                    bookmarkIcon.classList.add('bi-bookmark'); // Add the .bi-heart (empty heart) from classes in <i> element
-                }
-            });*/
     }
 }
